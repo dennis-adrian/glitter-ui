4fad72457fedd0a51c8c16addfbb3449f3f97769
dennis-adrian <dennisguzmanbo@gmail.com>
2023-11-07 00:47:59 -0400

    Updated version of the landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,47 @@
+import Button from '../components/Button'
+import { signInWithGoogle } from '../helpers/auth_helper'
+
 import mascot from '../assets/mascot.png'
+import glitter from '../assets/glitter.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons'
+
+import AccentCard from '../components/AccentCard'
+
 import './Home.scss'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
 const HomePage = () => {
   return (
     <>
       <section className='hero'>
-        <h1 className='hero-title'>Bienvenido a Glitter</h1>
-        <img className='hero-img' src={mascot} alt="glitter mascot" />
+        <img className='hero-heading' src={glitter} alt="glitter" />
+        <img className='hero-mascot' src={mascot} alt="glitter mascot" />
+        <h1 className='hero-title'>¡brillemos juntos!</h1>
+        <h2 className='hero-subtitle'>próximo evento</h2>
+        <AccentCard styles={{ fontWeight: 400, width: '70%' }}>
+          <span>Viernes 1 y sábado 2 de diciembre</span>
+          <span>10:00 a 20:00</span>
+          <span>Galería del CBA, calle sucre</span>
+        </AccentCard>
+        <Button onClick={() => signInWithGoogle()}>Crea tu cuenta</Button>
+        <p className='hero-text'>
+          Crea experiencias que te inspiren, conecta con otros artistas y celebra lo que eres 
+        </p>
+        <div className='social-media'>
+          <a href='https://instagram.com/glitter.bo'>
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href='#'>
+            <FontAwesomeIcon icon={faTiktok} />
+          </a>
+          <a href='#' referrerPolicy='no-referrer' target='_blank'>
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="mailto:glitter.festival.creativo@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </a>
+        </div>
       </section>
     </>
   )
