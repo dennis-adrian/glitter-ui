a591fddce4c547a058b4036ddd10b3ddc37210df
dennis-adrian <dennisguzmanbo@gmail.com>
2023-11-07 12:04:53 -0400

    Add desktop view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Button from '../components/Button'
 
 import mascot from '../assets/mascot.png'
+import mascotDesktop from '../assets/mascot_desktop.png'
 import glitter from '../assets/glitter.png'
 
 import AccentCard from '../components/AccentCard'
@@ -10,26 +11,29 @@ import './Home.scss'
 
 const HomePage = () => {
   return (
-    <>
-      <section className='hero'>
-        <img className='hero-heading' src={glitter} alt="glitter" />
-        <img className='hero-mascot' src={mascot} alt="glitter mascot" />
-        <h1 className='hero-title'>¡brillemos juntos!</h1>
-        <p className='hero-text'>
-          Crea experiencias que te inspiren, conecta con otros artistas y celebra lo que eres 
-        </p>
-      </section>
-      <section className='event-info' >
-        <h2 className='event-info-next-event'>próximo evento</h2>
-        <AccentCard styles={{ fontWeight: 400, width: '70%' }}>
-          <span>Viernes 1 y sábado 2 de diciembre</span>
-          <span>10:00 a 20:00</span>
-          <span>Galería del CBA, calle sucre</span>
-        </AccentCard>
-        <Button onClick={() => {}}>Crea tu cuenta</Button>
+    <section className='home-page'>
+      <img className='heading-img' src={glitter} alt="glitter" />
+      <section className='main-content'>
+        <section className='hero'>
+          <img className='hero-mascot' src={mascot} alt="glitter mascot" />
+          <img className='hero-mascot-desktop' src={mascotDesktop} alt="glitter mascot" />
+          <h1 className='hero-title'>¡brillemos juntos!</h1>
+          <p className='hero-text'>
+            Crea experiencias que te inspiren, conecta con otros artistas y celebra lo que eres 
+          </p>
+        </section>
+        <section className='event-info' >
+          <h2 className='event-info-next-event'>próximo evento</h2>
+          <AccentCard styles={{ fontWeight: 400 }}>
+            <span>Viernes 1 y sábado 2 de diciembre</span>
+            <span>10:00 a 20:00</span>
+            <span>Galería del CBA, calle Sucre #346</span>
+          </AccentCard>
+          <Button onClick={() => {}}>Crea tu cuenta</Button>
+        </section>
       </section>
       <Footer />
-    </>
+    </section>
   )
 }
 
