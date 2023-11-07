6cf52e2f42e2dcd6ac17c24c95b781e9baeb2070
dennis-adrian <dennisguzmanbo@gmail.com>
2023-11-07 10:18:58 -0400

    Group elements so it's easier to transition to a desktop view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,16 +15,18 @@ const HomePage = () => {
         <img className='hero-heading' src={glitter} alt="glitter" />
         <img className='hero-mascot' src={mascot} alt="glitter mascot" />
         <h1 className='hero-title'>¡brillemos juntos!</h1>
-        <h2 className='hero-subtitle'>próximo evento</h2>
+        <p className='hero-text'>
+          Crea experiencias que te inspiren, conecta con otros artistas y celebra lo que eres 
+        </p>
+      </section>
+      <section className='event-info' >
+        <h2 className='event-info-next-event'>próximo evento</h2>
         <AccentCard styles={{ fontWeight: 400, width: '70%' }}>
           <span>Viernes 1 y sábado 2 de diciembre</span>
           <span>10:00 a 20:00</span>
           <span>Galería del CBA, calle sucre</span>
         </AccentCard>
         <Button onClick={() => {}}>Crea tu cuenta</Button>
-        <p className='hero-text'>
-          Crea experiencias que te inspiren, conecta con otros artistas y celebra lo que eres 
-        </p>
       </section>
       <Footer />
     </>
