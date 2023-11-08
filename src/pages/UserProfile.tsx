48110bab0cbda0e6bdc59898fc15b35808109ebe
dennis-adrian <dennisguzmanbo@gmail.com>
2023-11-07 22:22:30 -0400

    Add some routes

---
+++ src/pages/UserProfile.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from 'react'
+import { redirect } from 'react-router-dom'
+
+const UserProfile = () => {
+  return (
+    <div>UserProfile</div>
+  )
+}
+
+export default UserProfile
