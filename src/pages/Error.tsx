eaf3cb3d3b9467c84320f58674d1289b0a3de0fd
dennis-adrian <dennisguzmanbo@gmail.com>
2023-11-08 23:26:35 -0400

    Fix some ts compiler errors

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,16 +1,15 @@
-import { useRouteError } from 'react-router-dom'
+import { ErrorResponse, useRouteError } from 'react-router-dom'
 import './Error.scss'
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+  const error = useRouteError() as ErrorResponse;
 
   return (
     <div className='error-page'>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error.statusText}</i>
       </p>
     </div>
   );
--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,6 +1,3 @@
-import { useEffect } from 'react'
-import { redirect } from 'react-router-dom'
-
 const UserProfile = () => {
   return (
     <div>UserProfile</div>
