6d9e95da1e6e07f46abe8ed6e2985e68da8eb0f8
dennis-adrian <dennisguzmanbo@gmail.com>
2023-11-17 16:49:32 -0400

    Add absolute paths config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      assets: '/src/assets',
+    },
+  },
   preview: {
     port: 8080,
     strictPort: true,
@@ -13,6 +20,6 @@ export default defineConfig({
     port: 8080,
     strictPort: true,
     host: true,
-    origin: 'http://0.0.0.0:8080'
+    origin: 'http://0.0.0.0:8080',
   },
-})
+});
